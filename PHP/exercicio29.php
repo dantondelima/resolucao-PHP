c51b56ed977c2dbd8766ba85716e7565730b0a63
dantondelima <dantondelima.contato@gmail.com>
2018-03-24 23:12:45 -0300

    exercicio 29 php parte

---
+++ PHP/exercicio29.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../CSS/estilo.css">
+    <title>Exercício 29</title>
+</head>
+<body>
+    <div>
+        <?php 
+            $vi = $_GET["vi"];
+            $acel = $_GET["acel"];
+            $tempo= $_GET["tempo"];
+
+            $vf = $vi + $acel * $tempo;
+            if ($vf <= 40) {
+                echo "Veículo muito lento";
+            }
+            elseif ($vf > 40 && $vf <= 60) {
+                echo "Velocidade permitida";
+            } 
+            elseif ($vf > 60 && $vf <= 80) {
+                echo "Velocidade de cruzeiro";
+            }
+        ?>
+    </div>
+</body>
+</html>
