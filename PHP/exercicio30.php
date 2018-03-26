efa079357534b42c2893f87905efa27852c666a0
dantondelima <dantondelima.contato@gmail.com>
2018-03-26 09:23:14 -0300

    exercicio 30 php inicio

---
+++ PHP/exercicio30.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../CSS/estilo.css">
+    <title>Exercício 30</title>
+</head>
+<body>
+    <div>
+        <?php 
+            $vi = $_GET["vi"];
+            $acel = $_GET["acel"];
+            $tempo= $_GET["tempo"];
+
+        ?>
+    </div>
+</body>
+</html>
