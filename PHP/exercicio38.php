24172dfee43546a6cf80a0bb17a00f9acd02cab2
dantondelima <dantondelima.contato@gmail.com>
2018-03-26 23:11:43 -0300

    exercicio 38 php inicio

---
+++ PHP/exercicio38.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../CSS/estilo.css">
+    <title>Exercício 30</title>
+</head>
+<body>
+    <div>
+        <?php 
+            $n1 = $_GET["n1"];
+            $n2 = $_GET["n2"];
+            $media = ($n1 + 2 * $n2) / 3;
+
+            if($media >= 5){
+                echo "A média é $media e a situação do aluno é: Aprovado";
+            }
+            else {
+                echo "A média é $media e a situação do aluno é: Reprovado";
+            }
+        ?>
+    </div>
+</body>
+</html>
