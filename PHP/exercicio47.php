43e6448c9c52d8b77f9fde9dcae528d2e54650b7
dantondelima <dantondelima.contato@gmail.com>
2018-04-07 04:12:22 -0300

    exercicio 47 php

---
+++ PHP/exercicio47.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../CSS/estilo.css">
+    <title>Exercício 42</title>
+</head>
+<body>
+    <div>
+        <form action="../PHP/exercicio47-1.php" method="GET">  
+            <label for="n">Digite um número de 1 a 20:</label>
+            <input type="number" name="n" min="1" max="20" step="1"><br><br>
+            <input type="submit" value="Gerar">
+    </form>
+    </div>
+</body>
+</html>
